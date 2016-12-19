e4343dfbc2e3813111e867cc03b9cdf424c580d2
Mstanton98 <mstanton98@gmail.com>
2016-12-19 13:41:28 -0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 8000;
+const server = require('http').createServer(app);
+const path = require('path');
+
+app.disable('x-powered-by');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  console.error(JSON.stringify(err, null, 2));
+
+  if (err.status) {
+    return res
+      .status(err.status)
+      .set('Content-Type', 'text/plain')
+      .send(err.statusText);
+  }
+
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+server.listen(port, () => {
+  console.log('Listening on port', port);
+});
