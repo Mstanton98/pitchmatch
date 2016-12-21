f555ef3a04aba7cf7e5ef1471b9b063b79d3086c
Mstanton98 <mstanton98@gmail.com>
2016-12-20 23:39:55 -0800

    added migrations

--- server/migrations/20161219135942_user_messages.js
+++ server/migrations/20161219135942_user_messages.js
@@ -1,8 +1,25 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('user_messages', (table) => {
+    table.increments();
+    table.integer('user_id')
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE')
+    .index();
+    table.integer('match_id')
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE')
+    .index();
+    table.string('message_body').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex) {
+  return knex.schema.dropTable('user_messages');
+}
--- server/migrations/20161219140012_user_matches.js
+++ server/migrations/20161219140012_user_matches.js
@@ -1,8 +1,25 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('user_matches', (table) => {
+    table.increments();
+    table.integer('user_id')
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE')
+    .index();
+    table.integer('match_id')
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE')
+    .index();
+    table.boolean('is_match').notNullable().defaultTo(false);
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex) {
+  return knex.schema.dropTable('user_matches');
+}
